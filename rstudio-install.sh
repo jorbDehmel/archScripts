bc6c03d97c8ed842ad1c4edd4c2a29255782e296
jorbDehmel <jdehmel@outlook.com>
2023-04-14 10:57:41 -0600

    Bug fix

--- rstudio-install.sh
+++ rstudio-install.sh
@@ -1,5 +1,5 @@
 git clone https://aur.archlinux.org/rstudio-desktop-bin.git
 cd rstudio-desktop-bin
 makepkg -si
-sudo pacman -Syu texlive-most texlive-latex-extra
+sudo pacman -Syu texlive-most
 echo "alias rstudio='/usr/lib/rstudio/rstudio &'" >> ~/.bashrc
