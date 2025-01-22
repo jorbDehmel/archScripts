ca2f475e2da1c875cee8404413da66624ba1b064
Jordan Dehmel <jdehmel@outlook.com>
2025-01-22 08:01:10 -0700

    Updated to also purge podman w/ docker

--- archClean.sh
+++ archClean.sh
@@ -4,6 +4,7 @@ yay -Qdtq | yay -Rs - --noconfirm
 yay -Scc --noconfirm
 pacman -Scc --noconfirm
 docker system prune -af
+podman system prune -af
 sudo trash-empty --all-users -f
 npm cache clean --force
 df -h
